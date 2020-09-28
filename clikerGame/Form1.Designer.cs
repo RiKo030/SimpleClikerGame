239250e10ef9cd443c2ba432bc9b279df0cd547f
Volodymyr Yaruchyk <riko03030303@gmail.com>
2020-09-28 11:22:52 +0300

    v.1

---
+++ clikerGame/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace clikerGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(253, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(547, 376);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(278, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(404, 24);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Урон";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(13, 154);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Золото";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(13, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Урон в секунду";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(342, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 22);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "HP :";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 220);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(235, 90);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Прокачать Клик урон";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 349);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(235, 89);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Прокачать Урон в секунду";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Death.png");
+            this.imageList1.Images.SetKeyName(1, "Ghost.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ clikerGame/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+using System.Collections.Specialized;
+using System.Threading;
+using System.Net.Http.Headers;
+
+namespace clikerGame
+{
+    public partial class Form1 : Form
+    {
+        Player player;
+        Enemy enemy;
+        Shop shop;
+        int paintPercent = 4;
+        int imagenumber = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.ReloadEnemy);
+            player = new Player();
+            enemy = new Enemy();
+            shop = new Shop();
+            button1.BackgroundImage = imageList1.Images[0];
+            timer1.Start();
+            UpdateData();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            enemy.DealDamage(player.damage);
+            int percent = paintPercent * (100 - (enemy.HP / (enemy.MaxHP / 100)));
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+            SolidBrush GreenBrush = new SolidBrush(Color.Green);
+            g.FillRectangle(GreenBrush, 1, 1, 400 - percent, 20);
+
+            Random r = new Random();
+            int x = r.Next(25, 375);
+            int y = r.Next(25, 320);
+            Point p = new Point(x, y);
+            Graphics b = button1.CreateGraphics();
+            b.DrawString("-" + player.damage + "CLICK", new Font("Arial", 16), new SolidBrush(Color.BlueViolet), p);
+
+            if (enemy.HP <= 0)
+            {
+                player.GetGold(enemy.price);
+                g.FillRectangle(GreenBrush, 1, 1, 400, 20);
+                enemy.LevelUp();
+                if (imagenumber == 0)
+                {
+                    button1.BackgroundImage = imageList1.Images[1];
+                    imagenumber = 1;
+                }
+                else if (imagenumber == 1)
+                {
+                    button1.BackgroundImage = imageList1.Images[0];
+                    imagenumber = 0;
+                }
+            }
+            UpdateData();
+
+        }
+
+        void ReloadEnemy(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            SolidBrush GreenBrush = new SolidBrush(Color.Green);
+
+            g.FillRectangle(GreenBrush, 1, 1, 400, 20);
+        }
+
+        void UpdateData()
+        {
+            label1.Text = "Клик урон = " + player.damage;
+            label2.Text = "Золото = " + player.gold;
+            label3.Text = "Урон в секунду = " + player.autodamage;
+            label4.Text = "HP: " + enemy.HP;
+            button2.Text = "Прокачать Клик урон за " + shop.price1 + " золотих";
+            button3.Text = "Прокачать Урон в секунду за " + shop.price2 + " золотих";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            if (player.BuyAttack(shop.price1))
+            {
+                shop.PriceUp1();
+                UpdateData();
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (player.BuyAutoAttack(shop.price2))
+            {
+                shop.PriceUp2();
+                UpdateData();
+            }
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            enemy.DealDamage(player.autodamage/2);
+            int percent = paintPercent * (100 - (enemy.HP / (enemy.MaxHP / 100)));
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+            SolidBrush GreenBrush = new SolidBrush(Color.Green);
+            g.FillRectangle(GreenBrush, 1, 1, 400 - percent, 20);
+
+            Random r = new Random();
+            int x = r.Next(25, 375);
+            int y = r.Next(25, 320);
+            Point p = new Point(x, y);
+            Graphics b = button1.CreateGraphics();
+            b.DrawString("-" + player.autodamage, new Font("Arial", 16), new SolidBrush(Color.Red), p);
+
+            if (enemy.HP <= 0)
+            {
+                player.GetGold(enemy.price);
+                g.FillRectangle(GreenBrush, 1, 1, 400, 20);
+                enemy.LevelUp();
+                if (imagenumber == 0)
+                {
+                    button1.BackgroundImage = imageList1.Images[1];
+                    imagenumber = 1;
+                }
+                else if (imagenumber == 1)
+                {
+                    button1.BackgroundImage = imageList1.Images[0];
+                    imagenumber = 0;
+                }
+            }
+            UpdateData();
+        }
+
+    }
+    public class Shop
+    {
+        public int price1 { get; private set; }
+        public int price2 { get; private set; }
+
+        public Shop()
+        {
+            price1 = 5;
+            price2 = 5;
+        }
+
+        public void PriceUp1()
+        {
+            price1 = price1 * 2;
+        }
+        public void PriceUp2()
+        {
+            price2 = price2 * 2;
+        }
+    }
+    public class Player
+    {
+        public int gold { get; private set; }
+        public int damage { get; private set; }
+        public int autodamage { get; private set; }
+    
+        public Player()
+        {
+            this.gold = 0;
+            this.damage = 50;
+            this.autodamage = 20;
+        }
+
+        public void GetGold(int gold)
+        {
+            this.gold += gold;
+        }
+
+        public bool BuyAttack(int price)
+        {
+            if(gold >= price)
+            {
+                gold -= price;
+                damage = damage * 2;
+                return true;
+            }
+            return false;
+
+        }
+
+        public bool BuyAutoAttack(int price)
+        {
+            if (gold >= price)
+            {
+                gold -= price;
+                autodamage = autodamage * 2;
+                return true;
+            }
+            return false;
+
+        }
+    }
+
+    public class Enemy
+    {
+        public int MaxHP { get; private set; }
+        public int HP { get; private set; }
+        public int price { get; private set; }
+
+        public Enemy()
+        {
+            this.MaxHP = 400;
+            this.price = 5;
+            HP = MaxHP;
+        }
+
+        public void DealDamage(int damage)
+        {
+            HP -= damage;
+        }
+
+        public void LevelUp()
+        {
+            MaxHP = MaxHP * 2;
+            price = price * 2;
+            HP = MaxHP;
+        }
+    }
+
+    
+}
